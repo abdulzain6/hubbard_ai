7030a19a269046ae6d74fd4dd82859d99763b6d3
Ubuntu <ubuntu@ip-172-31-30-146.ca-central-1.compute.internal>
2024-12-26 12:09:32 +0000

    fix

--- api/main.py
+++ api/main.py
@@ -36,5 +36,5 @@ if __name__ == "__main__":
         host="0.0.0.0",
         port=8000,
         ssl_keyfile="./key.pem",
-        ssl_certfile="./cert.pem"
+        ssl_certfile="./aacert.pem"
     )
